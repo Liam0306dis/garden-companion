5565d08310c10c22e0d7aa13fd34b16e1372c08e
liam0306dis <liam0306dis@users.noreply.github.com>
2026-07-29 23:29:12 +0100

    Bump Garden Companion to 0.6.50

--- dist/garden-companion.user.js
+++ dist/garden-companion.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Garden Companion
 // @namespace    https://github.com/Liam0306dis/garden-companion
-// @version      0.6.49
+// @version      0.6.50
 // @description  Manual garden tools, pet teams, alerts, timers, and room browsing
 // @author       Liam
 // @match        https://1227719606223765687.discordsays.com/*
